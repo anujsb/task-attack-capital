f0e35111069d0593b2761651480497ad7a71bb2c
anujsb <141896390+anujsb@users.noreply.github.com>
2025-04-18 14:38:50 +0530

    removed info component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className="min-h-screen ">
       <div className="flex items-center justify-center bg-gradient-to-br from-purple-100 to-blue-50 p-10 top-0 rounded-lg border-b border-accent">
-        <Info />
+        {/* <Info /> */}
       </div>
       <Blog />
     </div>
